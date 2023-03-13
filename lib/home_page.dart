d154da7e14eae1672b98f527edf2b055ef38bb10
c100bit <c100bit@gmail.com>
2023-03-13 21:41:20 +0300

    init commit

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_painter_clock_app/clock_view.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFF2D2F41),
+        child: ClockView(),
+        alignment: Alignment.center,
+      ),
+    );
+  }
+}
